8793b1f38a75b079a678c141050f8b8a3abdf0de
ES <2662442385@qq.com>
2025-12-23 16:20:13 +0800

    feat(VirtualWaterfall): enhance Test and TestHook components with mock data generation and improved card styling

--- packages/config/src/index.ts
+++ packages/config/src/index.ts
@@ -17,4 +17,9 @@ export const IMG_URLS = [
   'https://images.unsplash.com/photo-1682687220208-22d7a2543e88?q=80&w=1470&auto=format&fit=crop',
   'https://images.unsplash.com/photo-1686002359940-6a51b0d64f68?q=80&w=1374&auto=format&fit=crop',
   'https://images.unsplash.com/photo-1683009427513-28e163402d16?q=80&w=1470&auto=format&fit=crop',
+  'https://picsum.photos/400/600?random=1',
+  'https://picsum.photos/500/800?random=2',
+  'https://picsum.photos/300/500?random=3',
+  'https://picsum.photos/600/900?random=4',
+  'https://picsum.photos/350/550?random=5',
 ]
