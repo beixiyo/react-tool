52b45f91983b0d1a7153928093e7ed1f67c51d0f
CJL <2662442385@qq.com>
2026-01-31 17:20:35 +0800

    feat(Popover): add restore focus on open functionality
    - Introduced restoreFocusOnOpen prop to allow the Popover to return focus to the previously active element when opened, enhancing accessibility.
    - Updated PopoverProps to include the new property and modified the component logic to utilize the useRestoreFocusOnOpen hook.
    - Improved documentation for the Popover component to reflect the new feature.

--- packages/hooks/src/index.ts
+++ packages/hooks/src/index.ts
@@ -1,28 +1,14 @@
+export * from './dom'
 export * from './event'
 export * from './jotaiTool'
 export * from './lifecycle'
-
 export * from './memo'
 export * from './net'
 export * from './ob'
 export * from './ref'
 export * from './state'
-
 export * from './theme'
-
 export * from './timer'
 export * from './types'
-
-export * from './useAutoSave'
-export * from './useElBounding'
-
-export * from './useFloatingPosition'
-export * from './useGenRefState'
-
-export * from './useGetState'
 export * from './useNotifyParentReady'
-export * from './useStateWithPromise'
-export * from './useTextOverflow'
-export * from './useTheme'
-export * from './useViewportHeight'
 export * from './useWorker'
