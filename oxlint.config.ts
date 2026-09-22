00399088c3a026d8a7eb620440d8f56a0330fd8f
ES <2662442385@qq.com>
2026-09-22 19:57:36 +0800

    chore(lint): 暂时注释 docComment 规则

--- oxlint.config.ts
+++ oxlint.config.ts
@@ -9,14 +9,14 @@ export default defineConfig({
     'react/exhaustive-deps': 'warn',
     'react/rules-of-hooks': 'error',
     '@jl-org/eslint-plugins/forceTernary': 'warn',
-    '@jl-org/eslint-plugins/docComment': [
-      'warn',
-      {
-        /** 匹配文字字符开头，但跳过 lint、formatter、coverage 等工具指令 */
-        pattern:
-          '^(?!(?:(?:eslint|oxlint|stylelint)-(?:disable|enable)|(?:prettier|oxfmt|biome)-ignore|(?:istanbul|c8|v8) ignore)\\b)[A-Za-z0-9\\u00C0-\\u02AF\\u3040-\\u30FF\\u3400-\\u4DBF\\u4E00-\\u9FFF\\uAC00-\\uD7AF]',
-      },
-    ],
+    // '@jl-org/eslint-plugins/docComment': [
+    //   'warn',
+    //   {
+    //     /** 匹配文字字符开头，但跳过 lint、formatter、coverage 等工具指令 */
+    //     pattern:
+    //       '^(?!(?:(?:eslint|oxlint|stylelint)-(?:disable|enable)|(?:prettier|oxfmt|biome)-ignore|(?:istanbul|c8|v8) ignore)\\b)[A-Za-z0-9\\u00C0-\\u02AF\\u3040-\\u30FF\\u3400-\\u4DBF\\u4E00-\\u9FFF\\uAC00-\\uD7AF]',
+    //   },
+    // ],
     '@jl-org/eslint-plugins/reactVIf': 'error',
   },
 })
